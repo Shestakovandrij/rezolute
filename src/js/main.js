e8b995f4396cb71762812951e8d8e42c1ca124f2
Shestakovandrij <shestakovandrij@gmai.com>
2021-03-28 12:52:53 +0200

    adadad

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,9 @@ $(function(){
   $('.menu__btn').on('click', function(){
     $('.header__top').toggleClass('header__top--active')
   });
+  $('.menu__btn').on('click', function(){
+    $('.menu__btn').toggleClass('menu__btn--active')
+  });
 
 });
 
